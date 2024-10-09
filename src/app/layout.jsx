65e0a0f44f3d0bc6fbcd9b10f77e71caa8c88e4e
mohammad-jf <m.h.jalifathi@gmail.com>
2024-10-10 02:17:44 +0330

    some changes

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,3 +1,4 @@
+import Header from "@/components/layout/Header";
 import "../assets/styles/globals.css";
 import { Inter } from "next/font/google";
 
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Header />
         <main className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
           {children}
         </main>
