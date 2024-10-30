e07027bcdff4e44e701b40c6ee8f16ff8a21d216
mohammad-jf <m.h.jalifathi@gmail.com>
2024-10-30 11:09:40 +0330

    1.create session and login functionality 2.destroy session and logout functionality

--- src/actions/createSession.js
+++ src/actions/createSession.js
@@ -1,4 +1,6 @@
 "use server";
+import { createAdminClient } from "@/config/appwrite";
+import { cookies } from "next/headers";
 
 async function createSession(previousState, formData) {
   const email = formData.get("email");
@@ -10,8 +12,33 @@ async function createSession(previousState, formData) {
     };
   }
 
-  return {
-    success: true,
-  };
+  if (password.toString().length < 8) {
+    return {
+      error: "password must be longer than 8 characters",
+    };
+  }
+  
+  // get account instance
+  const { account } = await createAdminClient();
+  try {
+    // generate ssesion
+    const session = await account.createEmailPasswordSession(email, password);
+
+    // create cookie
+    cookies().set("appwrite-session", session.secret, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      expires: new Date(session.expire),
+      path: "/",
+    });
+
+    return {
+      success: true,
+    };
+  } catch (error) {
+    console.log("authentication Error :", error);
+    return { error: "Invalid credentials" };
+  }
 }
 export default createSession;
